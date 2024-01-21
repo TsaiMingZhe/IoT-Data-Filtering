c56f32c2a2751af394894ec4b981e9e4065199a9
TsaiMingZhe <143486025+TsaiMingZhe@users.noreply.github.com>
2024-01-21 17:41:13 +0800

    Add files via upload

---
+++ 00_TESTBED/define.v
@@ -0,0 +1,14 @@
+//state
+`define idle        0
+`define load        1
+`define DES         2
+`define get_crc     3
+`define conv_GB     4
+`define iot_out     5
+
+//function
+`define Encrypt     1
+`define Decrypt     2
+`define CRC_gen     3
+`define Bin2Gray    4
+`define Gray2Bin    5
